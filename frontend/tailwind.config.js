7fbcf0eced88d08216bc727d5c814c1abbd4edf0
Vicente Cao <139924476+vicent-cao02@users.noreply.github.com>
2025-11-12 14:34:11 -0500

    add frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadein: {
+          '0%': { opacity: '0', transform: 'translateY(24px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadein: 'fadein 0.7s cubic-bezier(.4,0,.2,1) forwards',
+      },
+    },
+  },
+  plugins: [],
+};
